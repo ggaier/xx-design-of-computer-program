c33d28e51702c91f3d3a73c5375088a17f2bf811
ggaier <jwenbo52@gmail.com>
2018-09-03 22:28:09 +0800

    finish shuffler test

--- lesson01/test_shuffler.py
+++ lesson01/test_shuffler.py
@@ -12,12 +12,10 @@ def test_shuffler(shuffler, deck = 'abcd', n=1000):
         for item in counts)
     name = shuffler.__name__
     print '%s(%s)%s' % (name, deck, ('ok' if ok else '***BAD***'))
+    for item, count in sorted(counts.items()):
+        print "%s:%4.1f" % (item, count*100./n)
+    print
 
-def test_shufflers(shufflers = [shuffle, shuffle1], decks = ['abc', 'ab']):
-    for deck in decks:
-        print 
-        for f in shufflers:
-            test_shuffler(f, deck)
 
 def factorial(n): return 1 if n<=1 else n*factorial(n-1)
 
@@ -30,10 +28,33 @@ def shuffle1(deck):
     while not all(swapped):
         i, j = random.randrange(N), random.randrange(N)
         swapped[i] = swapped[j] = True
-        swapp(deck, i, j)
+        swap(deck, i, j)
         
 
-def swapp(deck, i, j):
-    print 'swap ', i, j
+def swap(deck, i, j):
+    # print 'swap ', i, j
     deck[i], deck[j] = deck[j], deck[i]
 
+def shuffle2(deck):
+    N = len(deck)
+    swapped = [False]*N
+    while not all(swapped):
+        i, j = random.randrange(N), random.randrange(N)
+        swapped[i] = True
+        swap(deck, i, j)
+
+
+def shuffle3(deck):
+    N = len(deck)
+    for i in range(N):
+        swap(deck, i, random.randrange(N))
+
+
+def test_shufflers(shufflers=[shuffle, shuffle1, shuffle2, shuffle3], decks=['abc', 'ab']):
+    for deck in decks:
+        print
+        for f in shufflers:
+            test_shuffler(f, deck)
+
+
+test_shufflers()

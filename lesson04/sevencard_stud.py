9b3636991f448230d42302ca6885c7f84503b928
ggaier <jwenbo52@gmail.com>
2018-09-04 23:16:49 +0800

    handle jokers wild

--- lesson04/sevencard_stud.py
+++ lesson04/sevencard_stud.py
@@ -16,3 +16,34 @@ def test_best_hand():
     return 'test_best_hand passes'
 
 print test_best_hand()
+
+
+allranks = '23456789TJQKA'
+redcards = [r+s for r in allranks for s in 'DH']
+blackcards = [r+s for r in allranks for s in 'SC']
+
+def best_wild_hand(hand):
+    "Try all values for jokers in all 5-card selections."
+    # Your code here
+    hands = set(best_hand(h) for h in itertools.product(
+        *map(replacement, hand)))
+    return max(hands, key = hand_rank)
+
+def replacement(card):
+    if card == '?B':
+         return blackcards 
+    elif card == '?R':
+        return redcards
+    else:
+        [card]
+
+def test_best_wild_hand():
+    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+            == ['7C', '8C', '9C', 'JC', 'TC'])
+    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+            == ['7C', 'TC', 'TD', 'TH', 'TS'])
+    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_wild_hand passes'
+
+print test_best_hand()

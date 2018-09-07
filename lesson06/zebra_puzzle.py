6b5725435cd0ce9d2499243d7f874604fe2e25d0
ggaier <jwenbo52@gmail.com>
2018-09-08 00:05:41 +0800

    add nextto and imright function

--- lesson06/zebra_puzzle.py
+++ lesson06/zebra_puzzle.py
@@ -2,3 +2,17 @@
 
 #python handle small integers as identical, so use `is` also same as ==
 
+import itertools
+
+houses = [1, 2, 3, 4, 5]
+
+# permutations function: all possible full-length permutations are generated
+ordering = list(itertools.permutations(houses))
+
+def imright(h1, h2):
+    return (h1-h2)==1
+
+def nextto(h1, h2):
+    return abs(h1-h2) ==1
+
+print nextto(2, 3)
